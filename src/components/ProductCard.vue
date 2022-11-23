bc980c0f494de9a76510af50f2510f85df85aeed
Simone Kranawetter <simonekranawetter@gmail.com>
2022-11-23 18:13:39 +0100

    :tada: Start project

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="product-card">
+    <div>
+      <img src="../assets/Featured_Product.svg" alt="" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ProductCard",
+  // props: {
+  //  msg: String
+  //}
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
