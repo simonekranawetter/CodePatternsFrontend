721363facc0fb6d1a9c0d28a2c7ec0bec34a40fa
Simone Kranawetter <simonekranawetter@gmail.com>
2022-12-09 22:07:50 +0100

    :sparkles: make a rating component

---
+++ src/components/RatingComponent.vue
@@ -0,0 +1,32 @@
+<!-- eslint-disable prettier/prettier -->
+<template>
+  <div class="rating">
+    <icon-component
+      v-if="rating.length < 5"
+      class="icons"
+      icon="ion:star-outline"
+      color="salmon"
+      size="18"></icon-component>
+    <icon-component
+      v-if="rating.length"
+      class="icons"
+      icon="ion:star"
+      color="salmon"
+      size="18"></icon-component>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import IconComponent from "@/components/IconComponent.vue";
+
+export default defineComponent({
+  components: { IconComponent },
+  name: "RatingComponent",
+  props: {
+    rating: {
+      type: Object,
+      required: true,
+    },
+  },
+});
+</script>
